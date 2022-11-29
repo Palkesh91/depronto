fd071da2fa405c0e14c06acbf96883f2982de65c
Palkesh91 <palkeshdharmik91@gmail.com>
2022-11-30 03:26:43 +0530

    second commit

--- app.js
+++ app.js
@@ -21,6 +21,22 @@ app.get('/feedback' , (req,res) => {
     res.render("feedback");
 });
 
+app.post("/feedback" , (req,res) =>{
+    const user = {
+        name: req.body.name,
+        date: req.body.date,
+        feedbackTitle: req.body.title,
+        feedback: req.body.feedback,
+        category:{
+            asian:req.body.asian,
+            american:req.body.american,
+            other:req.body.other,
+        },
+        gender: req.body.gender,
+        
+    };
+    console.log(user);
+});
 
 app.listen(3000 , () => {
     console.log("server started on port 3000");
